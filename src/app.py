cc5f3b64c7f9be3cc4185fdb85cefca26d019de6
Luciferian Ink <LuciferianInk@protonmail.com>
2023-11-27 18:30:14 -0600

    fix bugs

--- src/app.py
+++ src/app.py
@@ -12,13 +12,17 @@ app = Flask(__name__)
 
 @app.route("/generate")
 def generate():
+    data = None
     try:
         kwargs = request.get_json()
         data = asyncio.run(
             async_generate_image(apikey=os.environ["HORDE_API_KEY"], **kwargs)
         )
+        if "err" in data:
+            raise Exception("There was an error.")
         return jsonify(data), 200
     except Exception as e:
+        logger.error(data)
         return jsonify(data), 204
 
 
