74623c88c2317a31e6a13f5f13d97a802467ae9e
Luciferian Ink <LuciferianInk@protonmail.com>
2024-06-02 06:29:26 -0500

    update to latest horde-sdk, suppressing warnings

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import traceback
 
 from flask import Flask, jsonify, request
 from loguru import logger
@@ -13,21 +12,23 @@ app = Flask(__name__)
 @app.route("/generate")
 def generate():
     kwargs = request.get_json()
+    code = 200
     data = asyncio.run(generate_image(apikey=os.environ.get("HORDE_API_KEY"), **kwargs))
     if "err" in data:
+        code = 400
         logger.error(data)
-        return jsonify(data), 400
-    return jsonify(data), 200
+    return jsonify(data), code
 
 
 @app.route("/caption")
 def caption():
     kwargs = request.get_json()
+    code = 200
     data = asyncio.run(caption_image(apikey=os.environ.get("HORDE_API_KEY"), **kwargs))
     if "err" in data:
+        code = 400
         logger.error(data)
-        return jsonify(data), 400
-    return jsonify(data), 200
+    return jsonify(data), code
 
 
 if __name__ == "__main__":
